f8e527e1c2b79f6c0c68761018dc3e3e4b08b0c0
FKorving <korving.f@gmail.com>
2019-03-23 13:20:24 +0200

    Adds config experment

--- .circleci/pushConf.sh
+++ .circleci/pushConf.sh
@@ -1,6 +1,15 @@
+# Full Build Creation (Needed with workflow orchestration)
 curl --user ${CIRCLE_TOKEN}: \
      --request POST \
-     --form revision=c35b2c8ee90cda4755810391add9d08085d734b1 \
-     --form config=@config.yml \
-     --form notify=false \
-       https://circleci.com/api/v1.1/project/github/Korving-F/thesis-cicd-examples/tree/master
+     --form revision=93227bb5d358f49f4764cc4e0341e602e2fefc5f \
+       https://circleci.com/api/v1.1/project/github/Korving-F/thesis-cicd-examples/build
+
+
+# Simple job trigger
+#curl --user ${CIRCLE_TOKEN}: \
+#     --request POST \
+#     --form config=@config.yml \
+#     --form notify=false \
+#     --form revision=93227bb5d358f49f4764cc4e0341e602e2fefc5f \
+#       https://circleci.com/api/v1.1/project/github/Korving-F/thesis-cicd-examples/tree/master
+
